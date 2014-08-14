42f025a17477c6fbb2d65c5deafa24833f1369db
ptdecker <ptdecker@mac.com>
2014-08-13 20:27:48 -0500

    Completed file watcher example

---
+++ watcher.js
@@ -0,0 +1,36 @@
+/*
+ * Exemplifies watching a file for changes then spawning a separate
+ * process to execute a shell function and process its subsequent
+ * output stream.
+ *
+ * c.f. "Node.js The Right Way", by Jim R. Wilson
+ */
+
+"use strict";
+
+const
+    fs = require('fs'),
+    spawn = require('child_process').spawn,
+    filename = process.argv[2];
+
+if (!filename) {
+    throw Error("A file to watch must be specified!");
+}
+
+fs.watch(filename, function() {
+
+    let ls = spawn('ls', ['-lh', filename]),
+        output = '';
+
+    ls.stdout.on('data', function(chunk) {
+        output += chunk.toString();
+    });
+
+    ls.on('close', function() {
+        let parts = output.split(/\s+/);
+        console.dir([parts[0], parts[4], parts[8]]);
+    });
+
+});
+
+console.log("Now watching 'target.txt' for changes...");
